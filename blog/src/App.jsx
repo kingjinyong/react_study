bdc06ad5403d2de19dc6737aaeca5bd5b0e1fece
kingjinyong <wlsdydwhswkf@gmail.com>
2025-10-31 19:30:39 +0900

    코딩애플 state 숙제 1

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -2,19 +2,32 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  
+
+  // a : useState안 값과 동일, b : state 변경 도와주는 함수  
   let post = '강남 우동 맛집'; 
-  // a : useState안 값과 동일, b : state 변경 도와주는 함수
-  let [글제목, b] = useState('남자 코트 추천');
+
   // state 쓰면 html은 자동 재렌더링 됨
   // 변동시 자동으로 html에 반영되게 만들고 싶으면 state쓰기
-  return (      // return() 안에는 병렬로 태그 2개 이상 기입금지
+  // 자주 변경될 것 같은 html 부분은 state로 만들어놓기
+  // [useState안 값과 동일, state 변경 도와주는 함수]  
+  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  
+  // return() 안에는 병렬로 태그 2개 이상 기입금지
+  return (      
       <div className="App">
         <div className="black-nav">
-          <h4>블로그임</h4>
+          <h4>ReactBlog</h4>
         </div>
         <div className = "list">
-          <h4>{글제목}</h4>
+          <h4>{글제목[0]}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+        <div className = "list">
+          <h4>{글제목[1]}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+        <div className = "list">
+          <h4>{글제목[2]}</h4>
           <p>2월 17일 발행</p>
         </div>
       </div>
